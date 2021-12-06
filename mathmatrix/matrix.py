a921dd340bfc59714135792fae37423b1c5da11e
Keerthi Vasan <sakeerthi23@gmail.com>
2021-12-06 18:59:42 +0530

    Addition and Subraction now return a Matrix instead of a list

--- mathmatrix/matrix.py
+++ mathmatrix/matrix.py
@@ -77,7 +77,8 @@ class Matrix:
                         return False
             return True
         else:
-            raise ValueError('Equality can only be checked between matrices or between a matrix and a 0')
+            raise ValueError(
+                'Equality can only be checked between matrices or between a matrix and a 0')
 
     def __add__(self, other):
         t = type(other)
@@ -87,9 +88,10 @@ class Matrix:
             if self.m != other.m or self.n != other.m:
                 raise ValueError(
                     'Matrix addition is only implemented for matrices with the same order')
-            return [[self[i][j] + other[i][j] for j in range(self.n)] for i in range(self.m)]
+            return Matrix(self.m, self.n, [[self[i][j] + other[i][j] for j in range(self.n)] for i in range(self.m)])
         else:
-            raise ValueError('Addition can only be checked between matrices or between a matrix and a nubmer (int or float)')
+            raise ValueError(
+                'Addition can only be checked between matrices or between a matrix and a nubmer (int or float)')
 
     def __sub__(self, other):
         t = type(other)
@@ -99,9 +101,10 @@ class Matrix:
             if self.m != other.m or self.n != other.m:
                 raise ValueError(
                     'Matrix addition is only implemented for matrices with the same order')
-            return [[self[i][j] - other[i][j] for j in range(self.n)] for i in range(self.m)]
+            return Matrix(self.m, self.n, [[self[i][j] - other[i][j] for j in range(self.n)] for i in range(self.m)])
         else:
-            raise ValueError('Subraction can only be checked between matrices or between a matrix and a nubmer (int or float)')
+            raise ValueError(
+                'Subraction can only be checked between matrices or between a matrix and a nubmer (int or float)')
 
     def __truediv__(self, other):
         t = type(other)
